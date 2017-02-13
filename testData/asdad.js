b304170587fa436896cc2c9065ada5be64ed3c73
unknown <simoyixuan890912@163.com>
2017-02-13 19:30:56 +0800

    2.13

---
+++ testData/asdad.js
@@ -0,0 +1,493 @@
+{
+    "ServerGroupList"
+:
+    [{
+        "environment": 1,
+        "liveGivePropertyCgiPrefix": "http://livepay.video.qq.com/fcgi-bin/give_tools?",
+        "liveProsListCgiPrefix": "http://livepay.video.qq.com/fcgi-bin/get_tools?"
+    }], "autoLoadUpdateLib"
+:
+    1, "autoUpdateDex"
+:
+    1, "autoUpdateLib"
+:
+    1, "bullet_config"
+:
+    {
+        "open"
+    :
+        1
+    }
+,
+    "cgi_config_new"
+:
+    {
+        "mirrorSearchCgiPrefix"
+    :
+        "http://s.video.qq.com/srh_aphone?", "searchHintCgiPrefix"
+    :
+        "http://s.video.qq.com/smt_aphone?"
+    }
+,
+    "confid"
+:
+    9990001, "debug_config"
+:
+    {
+        "reportLog"
+    :
+        0, "reportLogFirstloadingTimeMax"
+    :
+        30000, "reportLogSecondBuffingTimesMax"
+    :
+        100, "try_restart_on"
+    :
+        1, "xiaomi_tv_switch"
+    :
+        -1
+    }
+,
+    "dexLoadEnable"
+:
+    1, "doNotShowexclusiveLogo"
+:
+    0, "em"
+:
+    "", "expand"
+:
+    {
+        "iResearchSdkOnOff"
+    :
+        1
+    }
+,
+    "fmt"
+:
+    "sd", "general_app_config"
+:
+    {
+        "config_int"
+    :
+        {
+            "FansgroupUnreadNumReqTime"
+        :
+            180, "app_upgrade_guide_toggle"
+        :
+            1, "com.tencent.weishi"
+        :
+            1, "download_allow_shd"
+        :
+            1, "game_apk_download_mode"
+        :
+            0, "h5_game_jce_data_switch"
+        :
+            1, "hboControllSwitchOff"
+        :
+            0, "hollywood_auto_charge_switch"
+        :
+            2, "log_report_sample_value"
+        :
+            1, "log_report_vspi_download_report"
+        :
+            1, "log_report_vspi_play_report"
+        :
+            1, "menu_layout_fans_visible"
+        :
+            1, "mini_video_shot_cut_time"
+        :
+            8000, "mttCoreOrX5Core"
+        :
+            0, "mttCoreOrX5CoreV2"
+        :
+            1, "offline_cache_drm_open"
+        :
+            1, "p2p_download_type"
+        :
+            3, "personalized_wifi_is_auto_play"
+        :
+            1, "poster_ad_gallery_auto_scroll_period"
+        :
+            5000, "property_my_property_swtich"
+        :
+            0, "share_circle_key"
+        :
+            1, "star_list_refresh_from_internet_interval"
+        :
+            0, "star_share_switch"
+        :
+            1, "tv_project_enable"
+        :
+            0, "unicomEnableState"
+        :
+            1, "unicomH5Enable"
+        :
+            1, "update_show_normal_button"
+        :
+            1, "video_detail_wifi_is_auto_play"
+        :
+            1, "webapp_hollywood_charged_vip"
+        :
+            1, "weixin_timeline_share_screen"
+        :
+            1, "wifi_manager_open_tips_enable"
+        :
+            "1", "wx_image_share_enable"
+        :
+            0
+        }
+    ,
+        "config_string"
+    :
+        {
+            "china_mobile_imsi_head"
+        :
+            "46000,46002,46007,46008", "china_railcom_imsi_head"
+        :
+            "46020", "china_telecom_imsi_head"
+        :
+            "46003,46005,46011", "china_unicom_imsi_head"
+        :
+            "46001,46006,46009", "fixed_channel_id"
+        :
+            "100101|100191", "httpUserAgent"
+        :
+            "qqlive4Android/3.7.1.6061  Dalvik (Android 4.2.1)", "local_scan_target_dir"
+        :
+            "/tencent/QQfile_recv|/微云保存的文件|/BaiduNetdisk|/yunpan|/LetvDownload/storage/download", "net_check_url"
+        :
+            "http://ping.huatuo.qq.com", "property_pay_coin_fail"
+        :
+            "抱歉，钻石兑换V币服务已停止", "pull_ipad_text_in_cache"
+        :
+            "腾讯视频iPad版，限时免费加速下载", "unicomH5PageUrl"
+        :
+            "http://m.v.qq.com/activity/wo/free.html", "update_cancel_download_text"
+        :
+            "暂不升级", "update_normal_download_text"
+        :
+            "立即升级", "update_yyb_download_text"
+        :
+            "", "video_share_prefix"
+        :
+            "", "video_share_suffix"
+        :
+            " ", "video_shot_share_jump_icon"
+        :
+            "朋友圈", "webp_format_parameter"
+        :
+            "tp=webp"
+        }
+    }
+,
+    "general_config"
+:
+    {
+        "auto_play_ad"
+    :
+        0, "auto_play_video"
+    :
+        0, "check_wifi_config"
+    :
+        0, "cover_click_time"
+    :
+        0, "cover_frequency"
+    :
+        99999, "cover_shotest_interval"
+    :
+        500, "hardware_accelerate"
+    :
+        1, "open_gesture_return"
+    :
+        1, "push_switch"
+    :
+        0, "refresh_interval"
+    :
+        900, "retry_time"
+    :
+        {
+            "first"
+        :
+            5, "second"
+        :
+            5, "third"
+        :
+            9
+        }
+    }
+,
+    "guid"
+:
+    "503ee78d0cde1035a4d780fbd48ad00a", "httpproxy_config"
+:
+    {
+        "AdjustHttpBufferTimeFrame"
+    :
+        "1:10:240 210 120 90,15:17:240 210 120 90", "AllowAndroidUpload"
+    :
+        1, "CgiEnableHttps"
+    :
+        0, "ChargeVideoAddTime"
+    :
+        70, "HttpDownloadTimeFrame"
+    :
+        "2:10", "HttpFirstBufferTime"
+    :
+        120, "HttpFirstBufferTimeNotPlay"
+    :
+        30, "HttpSecondWarnTime"
+    :
+        90, "HttpSecondWarnTimeNotPlay"
+    :
+        15, "HttpTimeOutForSendRequest"
+    :
+        3000, "IsChargeOptimization"
+    :
+        1, "MaxLimitQuerySeedFrequencyIndex"
+    :
+        1, "MaxPeerPoolNum"
+    :
+        50, "MaxQueryWindowsPSCounter"
+    :
+        2, "MaxSeedNumFromWindows"
+    :
+        10, "MaxSeedNumPerFile"
+    :
+        10, "MaxUploadSeedNum"
+    :
+        10, "MoreMemoryExtendFactor"
+    :
+        20, "MoreMemoryMinRemainMemorySize"
+    :
+        500, "OfflineClipMaxTaskNumber"
+    :
+        4, "OfflineHttpDownSize"
+    :
+        960, "OfflineMaxDownSpeedTimeFrame"
+    :
+        "0:21:20000 21:23:200 23:24:20000", "OfflineMaxDownSpeedTimeFrameByMin"
+    :
+        "19:00,22:30,120 00:00,00:00,120", "OfflineMaxSeedPerFile"
+    :
+        30, "OfflineMaxSpeedFactorTimeFrame"
+    :
+        "0:1:50:120:10 12:15:50:120:10 18:24:50:120:10", "OfflineMaxSpeedProtect"
+    :
+        200, "OfflineMinHttpSpeedForDown"
+    :
+        1, "OfflineRestOfP2pDownTime"
+    :
+        3, "OfflineResumeRisk"
+    :
+        5, "OfflineResumeSpeedTime"
+    :
+        60, "OfflineSlideWindowSize"
+    :
+        "5 32 64", "OfflineTestDownSpeedTime"
+    :
+        0, "OfflineUseP2PTimeFrame"
+    :
+        "0:1 12:24", "OnlineResumeRisk"
+    :
+        1, "P2PLiveConfig"
+    :
+        "", "P2PMaxCacheSize"
+    :
+        20, "P2PReportUseUDP"
+    :
+        0, "PSDoHeartBeatInterval"
+    :
+        20, "PunchMaxPunchTimes"
+    :
+        2, "PunchTimeout"
+    :
+        5000, "ReleaseMemoryStrategy"
+    :
+        0, "SeedNumPerFileStopDeletePeer"
+    :
+        0, "connect_timeout"
+    :
+        3000, "download_max_retry"
+    :
+        5, "download_retry_interval"
+    :
+        2, "enable_async_dns"
+    :
+        0, "first_block_size"
+    :
+        1048576, "force_none_p2p"
+    :
+        0, "hevclv"
+    :
+        0, "max_concurrent_connections"
+    :
+        5, "max_hash_errors"
+    :
+        5, "max_reconnect"
+    :
+        3, "max_redirect"
+    :
+        3, "md5_check_enable"
+    :
+        1, "md5_check_fail_retry_time"
+    :
+        2, "min_buffer_time"
+    :
+        30, "play_checksum"
+    :
+        0, "play_header_factor"
+    :
+        256, "play_short_video_size"
+    :
+        30, "play_standby_length"
+    :
+        3, "playbuffer_length"
+    :
+        100, "playbuffer_prefetch_factor"
+    :
+        10, "playbuffer_prefetch_length"
+    :
+        300, "playbuffer_prefetch_low"
+    :
+        10, "prefer_p2p"
+    :
+        1, "reconnect_timeout"
+    :
+        3000, "tpt_max_retries"
+    :
+        3, "tpt_server_address"
+    :
+        "downloadtpt.qqlive.qq.com", "tpt_server_port"
+    :
+        443, "use_new_p2p"
+    :
+        1
+    }
+,
+    "msg"
+:
+    "", "player_config"
+:
+    {
+        "vod_fmt"
+    :
+        "5min_mp4"
+    }
+,
+    "rules"
+:
+    [{"c_id": "1", "c_name": "live_playurl", "c_rule": "http://v.qq.com/live/index.html?cnlid={cnlid}"}, {
+        "c_id": "2",
+        "c_name": "cover_web_playurl_3g",
+        "c_rule": "http://3g.v.qq.com/android/player.html?cid={cid}&type={type}"
+    }, {
+        "c_id": "3",
+        "c_name": "video_in_cover_web_playurl_3g",
+        "c_rule": "http://3g.v.qq.com/android/player.html?cid={cid}&type={type}&vid={vid}"
+    }, {
+        "c_id": "4",
+        "c_name": "video_web_playurl_3g",
+        "c_rule": "http://3g.v.qq.com/android/player.html?type={type}&vid={vid}&title={title}"
+    }], "s"
+:
+    "o", "setting_plugin"
+:
+    [{"configName": "调查问卷", "contentUrl": "http://exp.qq.com/ur/?urid=24986"}], "statistic_config"
+:
+    {
+        "buffer_threshold"
+    :
+        6, "report_port"
+    :
+        80, "report_timeout"
+    :
+        5000, "report_uv_sync"
+    :
+        0
+    }
+,
+    "support_format"
+:
+    {
+        "fmt"
+    :
+        "sd"
+    }
+,
+    "viptab"
+:
+    {
+        "animation"
+    :
+        {
+            "actionUrl"
+        :
+            "txvideo://v.qq.com/Html5Activity?url=http%3A%2F%2Fcy.qq.com%2Fp%2Fcaiyun%2F312.html", "content"
+        :
+            "开通会员，4大特权带你玩转芈月传", "id"
+        :
+            3, "showTime"
+        :
+            10, "times"
+        :
+            2
+        }
+    ,
+        "tabbg"
+    :
+        {
+            "iconNormal"
+        :
+            "", "iconSelected"
+        :
+            ""
+        }
+    }
+,
+    "webview_config"
+:
+    {
+        "download_whitelist"
+    :
+        {
+            "host"
+        :
+            ["dldir1.qq.com", "theme.html5.qq.com", "theme.store.cs0309.html5.qq.com", "softfile.3g.qq.com", "www.webankcdn.net"], "url"
+        :
+            ["http://dd.myapp.com/16891/97D3F13B5FF0B23C5347FCD3AC8971A0.apk?fsname=com.tencent.tmgp.rxcq_1.0.2.568_568.apk", "http://wifi.pingan.com/android/channel/PinganWiFi-pp_qqlive.apk", "http://pregdownload.babytreeimg.com/foto3/common_photo/original/2016/0804/892bfcef28102840.apk"]
+        }
+    ,
+        "scheme_blackList"
+    :
+        ["qiyimobile", "tudou", "youku", "letvclient", "sohuvideo"], "sources"
+    :
+        [{
+            "cnName": "爱奇艺",
+            "jumpType": "0",
+            "source": "qiyi",
+            "ua": "Mozilla/5.0 (Linux; U; Android 4.2.2; zh-cn) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"
+        }], "urls"
+    :
+        [{
+            "cnName": "爱奇艺",
+            "jumpType": "0",
+            "ua": "Mozilla/5.0 (Linux; U; Android 4.2.2; zh-cn) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+            "url": "iqiyi.com"
+        }]
+    }
+,
+    "weibo_short_url"
+:
+    {
+        "weibo_short_url_circle"
+    :
+        0, "weibo_short_url_friend"
+    :
+        0, "weibo_short_url_mobile_qq"
+    :
+        0
+    }
+,
+    "weibo_ver"
+:
+    1
+}
